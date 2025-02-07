42a06fa8de81982f18bb9fc7b4832dcae7ba5be3
Maia Rowe <mrowe7000@icloud.com>
2025-02-07 18:49:52 +0000

    final

--- app.rb
+++ app.rb
@@ -1,19 +1,15 @@
 require "sinatra"
-require 'dotenv/load'
+require "dotenv/load"
 require "sinatra-contrib"
-require 'rspotify'
+require "rspotify"
 
-RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
 
-
-
-RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
-
-get '/' do
+get "/" do
   erb :form
 end
 
-post '/results' do
+post "/results" do
   artist_name = params[:artist_name]
   @artists = RSpotify::Artist.search(artist_name)
   erb :results
