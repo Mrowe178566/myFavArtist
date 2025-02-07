5d69343ac44c1a28f6920f7ab7403e6da3b70936
Maia Rowe <mrowe7000@icloud.com>
2025-02-07 11:00:17 +0000

    erb and rb creation

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require "sinatra"
-require "sinatra/reloader"
+require 'dotenv/load'
+require 'rspotify'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+
+get '/' do
+  erb :form
+end
+
+post '/results' do
+  artist_name = params[:artist_name]
+  @artists = RSpotify::Artist.search(artist_name)
+  erb :results
 end
